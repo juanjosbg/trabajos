5a089e64f20703bb97dd9ccd222c1e2e12960b5b
juanjosbg <juan_jose.borrero@uao.edu.co>
2018-10-21 13:43:33 -0500

    Trabajo numero dos

---
+++ new trabajo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Estudiantes
+{
+    public class new_trabajo
+    {
+        public new_trabajo()
+        {
+            string nombre;
+            int edad;
+
+            if 12^
+
+        }
+    }
+}
