0980c534a6ab05ac3748f44cc86f11a3207a3efe
juanjosbg <juan_jose.borrero@uao.edu.co>
2018-11-12 21:54:29 -0500

    correccion_trabjo2

---
+++ History_work_2/Correccion/Correccion_trabajo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Application 
+{
+
+//correccion del ejercicio 
+
+    public class Segundo_trabajo
+    {
+        public Segundo_trabajo()
+        {
+            Console.WriteLine("escriba la cantidad de (*) que desee en numeros enteros");
+
+            int numero = Convert.ToInt32(Console.ReadLine());
+            String n = "";
+            for (int i = 0; i < numero; i++)
+            {
+                n += " ";
+                Console.WriteLine(n + "*");
+            }
+    }
+}
