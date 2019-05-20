222701df3089adad5114624c16ee7ce5b11c4c12
juanjosbg <juan_jose.borrero@uao.edu.co>
2019-05-19 23:23:24 -0500

    history_work_5

---
+++ History_work_6/src/main/java/com/mycompany/complemento_juan/Main.java
@@ -0,0 +1,9 @@
+
+package com.mycompany.complemento_juan;
+
+public class Main{
+    public static void main(String[] args) {
+      ImprimirAsteriscos imprimir = new ImprimirAsteriscos();
+      imprimir.ejecutar();
+    }
+}
