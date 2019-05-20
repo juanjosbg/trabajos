011171f1e6be79dfe5ceddb5bf1bb3fc0ca7cc15
juanjosbg <juan_jose.borrero@uao.edu.co>
2019-05-20 17:56:15 -0500

    History_Work_5

--- history_work_5/src/main/java/com/mycompany/complemento_juan/Main.java
+++ history_work_5/src/main/java/com/mycompany/complemento_juan/Main.java
@@ -1,15 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.complemento_juan;
 
-public class Main 
-{
+public class Main {
+    
     public static void main(String[] args) {
       ImprimirAsteriscos imprimir = new ImprimirAsteriscos();
       imprimir.ejecutar();
     }
+    
 }
-   
